15dc666538245d0affcb83607f49cb7a2aa31971
OssielFabianCanulMis <OssielFabianCanulMis@users.noreply.github.com>
2019-11-21 12:33:18 -0500

    commit

--- app/src/main/java/com/itsfcp/proyecto4x3/vista4x3_notas.java
+++ app/src/main/java/com/itsfcp/proyecto4x3/vista4x3_notas.java
@@ -16,7 +16,7 @@ public class vista4x3_notas extends AppWidgetProvider {
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.vista4x3_notas);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
