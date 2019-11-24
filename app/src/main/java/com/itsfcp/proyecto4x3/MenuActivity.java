b42b91f0eb1c0b44a88ba24d233a6a4c9c4e6a4e
Slender5 <jafet.ramsell@gmail.com>
2019-11-24 01:32:27 -0500

    Cambios en el menu de personalizacion

--- app/src/main/java/com/itsfcp/proyecto4x3/MenuActivity.java
+++ app/src/main/java/com/itsfcp/proyecto4x3/MenuActivity.java
@@ -6,17 +6,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
     Button btnFondo;
     Button btnTexto;
+    private TextView mostrarPorcentaje;
+    private SeekBar seekBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         btnFondo = findViewById(R.id.btnFondo);
         btnTexto = findViewById(R.id.btnFondoText);
+        mostrarPorcentaje = (TextView)findViewById(R.id.txtCargar);
+        seekBar = (SeekBar)findViewById(R.id.seekBarFondo);
         cambios();
+        seekBar.setProgress(0);
+        seekBar.setMax(100);
+
+        seekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    //hace un llamado a la perilla cuando se arrastra
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar,
+                                                  int progress, boolean fromUser) {
+                        mostrarPorcentaje.setText(String.valueOf(progress)+" %");
+                    }
+                    //hace un llamado  cuando se toca la perilla
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+                    //hace un llamado  cuando se detiene la perilla
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                });
+
     }
 
     private void cambios(){
